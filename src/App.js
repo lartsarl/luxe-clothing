c47cf7795632045c0cf9c79e69e71b6c2c7afda5
URIS2019_ITjevci <lukasaric.saric997@gmail.com>
2020-04-14 10:46:35 +0200

    Added react-router and set test routes for the menu items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom'
+import HomePage from './pages/homepage/homepage.component';
+
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+
+const TestPage = (props) => (
+  <div>
+      <h1>TEST PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route exact path='/shop/hats' component={TestPage}></Route>
+        <Route exact path='/shop/jackets' component={TestPage}></Route>
+        <Route exact path='/shop/sneakers' component={TestPage}></Route>
+        <Route exact path='/shop/womens' component={TestPage}></Route>
+        <Route exact path='/shop/mens' component={TestPage}></Route>
+      </Switch>
     </div>
   );
 }
