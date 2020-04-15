3d241f47318cb49d3ec8b0caa7bf6f3c5e98b13a
lartsarl <lukasaric.saric997@gmail.com>
2020-04-15 18:19:07 +0200

    Added background for SignInAndSignUpPage

--- src/pages/sign-in-and-sing-up/sign-in-and-sing-up.component.jsx
+++ src/pages/sign-in-and-sing-up/sign-in-and-sing-up.component.jsx
@@ -3,11 +3,24 @@ import './sign-in-and-sing-up.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component'
 import SignUp from '../../components/sign-up/sign-up.component'
 
-const SignInAndSignUpPage = () => (
-    <div className='sign-in-and-sign-up'>
-        <SignIn></SignIn>
-        <SignUp></SignUp>
-    </div>
-) 
+class SignInAndSignUpPage extends React.Component {
+    render() {
+        return (
+            <div className='sign-in-and-sign-up'>
+                <SignIn></SignIn>
+                <SignUp></SignUp>
+            </div>
+        )
+    }
+    
+    componentWillMount(){
+        document.body.style.backgroundImage = `url(https://i.postimg.cc/L6hZqzYK/margo-robbi-margot-robbie-blondinka-krasotka-stoit-u-steny-v.jpg)`;
+        document.body.style.backgroundSize = 'cover';
+    }
+
+    componentWillUnmount(){
+        document.body.style.backgroundImage = null;
+    }
+}
 
 export default SignInAndSignUpPage;
