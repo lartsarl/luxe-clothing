d6c8ec08fa15e870d6104f0aa9b32ef29f5c323f
lartsarl <lukasaric.saric997@gmail.com>
2020-04-16 14:33:08 +0200

    Finished Cart component, implemented CartDropdown component to show all products in cart and update automatically

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component'
 import './cart-dropdown.styles.scss'
+import CartItem from '../cart-item/cart-item.component'
+import {connect} from 'react-redux'
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
-        <div className='class-items'></div>
+        <div className='cart-items'>
+            {
+                cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem}></CartItem>))
+            }
+        </div>
         <CustomButton> GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropdown;
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './cart-item.styles.scss'
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+    <div className='cart-item'>
+        <img src={imageUrl} alt={name}></img>
+        <div className='item-details'>
+            <span className='name'>{name}</span>
+            <span className='price'>${price} x ${quantity}</span>
+        </div>
+    </div>
+)
+
+export default CartItem;
