d6ef09dffe8479c47f7fd780ac14ff18f1d549fd
lartsarl <lukasaric.saric997@gmail.com>
2020-04-16 13:12:42 +0200

    Added styling to the ShopPage

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component'
+
 import './collection-item.styles.scss';
 
 const CollectionItem = ({id, name, price, imageUrl}) => (
@@ -12,6 +14,7 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
             <span className='name'>{name}</span>
             <span className='price'>${price}</span>
         </div>
+        <CustomButton inverted>ADD TO CART</CustomButton>
     </div>
 )
 
