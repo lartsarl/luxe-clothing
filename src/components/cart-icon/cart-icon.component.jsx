0a1c26fa30d7356dead0fdf67eff2f3ebf4d5bb5
lartsarl <lukasaric.saric997@gmail.com>
2020-04-17 12:30:13 +0200

    Implemented selector for Cart component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import {ReactComponent as ShoppingIcon} from '../../assets/Shopping-Bag.svg';
+import { ReactComponent as ShoppingIcon } from '../../assets/Shopping-Bag.svg';
 import './cart-icon.styles.scss';
-import {connect} from 'react-redux'
-import {toogleCartHidden} from '../../redux/cart/cart.actions'
+import { connect } from 'react-redux'
+import { toogleCartHidden } from '../../redux/cart/cart.actions'
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors'
 
-const CartIcon = ({toogleCartHidden}) => (
+const CartIcon = ({ toogleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toogleCartHidden}>
         <ShoppingIcon className='shopping-icon'></ShoppingIcon>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 
 const mapDispatchToProps = dispatch => ({
     toogleCartHidden: () => dispatch(toogleCartHidden())
+});
+
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
