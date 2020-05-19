772217beb13ea4fa016814c3c2282ddc97bf233b
URIS2019_ITjevci <lukasaric.saric997@gmail.com>
2020-05-19 22:04:04 +0200

    Implemented routing for shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
 
 import CollectionsOverview from '../../components/colelctions-overview/collections-overview.component'
+import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
     <div className='shop-page'>
-        <CollectionsOverview></CollectionsOverview>
+        <Route exact path={`${match.path}`} component={CollectionsOverview}></Route>
+        <Route path={`${match.path}`} component={CollectionPage}></Route>
     </div>
 );
 
